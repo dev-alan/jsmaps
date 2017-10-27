a0c115393b14241ae02d0c8405fb8c354a5f377e
Alan <alanl@sitesuite.com.au>
2017-10-27 15:00:07 +1100

    structured output folder

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,4 @@
-require('./assets/styles.less');
+// import './bootstrap/js/dist/util';
+import 'bootstrap/scss/bootstrap-reboot.scss';
+import 'bootstrap/scss/bootstrap-grid.scss';
+import './assets/styles.less';
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,56 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 let homepage = new HtmlWebpackPlugin({
   title: 'home',
-  template: './src/index.ejs'
+  template: './src/index.ejs',
+  filename: '../index.html'
 });
+let styles = new ExtractTextPlugin('styles.css')
 
 module.exports = {
   entry: './src/entry.js',
   output: {
-    path: __dirname + '/dist',
-    filename: "bundle.min.js"
+    path: __dirname + '/dist/assets/',
+    filename: 'bundle.min.js'
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'html-loader'
+            }
+          ]
+        })
       },
       {
         test: /.less$/,
-        use: [
-          {
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader'
-          }, {
-            loader: 'less-loader'
-          }
-        ]
+        use: styles.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            }, {
+              loader: 'less-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /.(sass|scss)$/,
+        use: styles.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            }, {
+              loader: 'sass-loader'
+            }
+          ]
+        })
       },
       {
         test: /\.css$/,
@@ -39,17 +58,27 @@ module.exports = {
           // path.resolve(__dirname, "app/src"),
           // path.resolve(__dirname, "app/test")
         ],
-        use: [
-          {
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader'
-          }
-        ]
+        use: styles.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            }
+          ]
+        })
       }
     ]
   },
   plugins: [
-    homepage
+    homepage,
+    styles,
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+      // In case you imported plugins individually, you must also require them here:
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+    })
   ]
 };
