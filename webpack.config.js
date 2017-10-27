f04babfbb77743c565a33c96185d68868d86c2fe
Alan <alanl@sitesuite.com.au>
2017-10-27 13:16:01 +1100

    added html templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,55 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+let homepage = new HtmlWebpackPlugin({
+  title: 'home',
+  template: './src/index.ejs'
+});
 
 module.exports = {
-  entry: "./src/entry.js",
+  entry: './src/entry.js',
   output: {
     path: __dirname + '/dist',
-    filename: "bundle.js"
+    filename: "bundle.min.js"
   },
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader'
+          }
+        ]
+      },
       {
         test: /\.css$/,
         include: [
           // path.resolve(__dirname, "app/src"),
           // path.resolve(__dirname, "app/test")
         ],
-        loader: 'style!css'
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    homepage
+  ]
 };
